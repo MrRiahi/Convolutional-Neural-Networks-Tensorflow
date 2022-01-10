873b3b8efd7533b94b81c422a88c416ed0bbbea0
MrRiahi <mohammad.r.riahi@gmail.com>
2022-01-10 22:33:59 +0330

    Update config file

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 
 class Config:
 
-    INPUT_SIZE = (64, 64)
+    RESNET50_INPUT_SIZE = (64, 64)
+    MOBILENET_V1_INPUT_SIZE = (224, 224)
+
     CLASSES = 101
 
     AUTO = tf.data.AUTOTUNE
