844a07aab48f382e968cb33df9f4d1607bbee3ff
MrRiahi <mohammad.r.riahi@gmail.com>
2022-01-10 22:34:49 +0330

    Update main to use MobileNetV1

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from tensorflow.keras.losses import CategoricalCrossentropy
 import tensorflow_datasets as tfd
 
-from config import Config
+from config import Config as Cfg
 from ResNet import ResNet50
+from MobileNet import MobileNetV1
 from preprocess import preprocess_caltech101
 
 # Load caltech101 dataset
 (ds_train, ds_test), ds_info = tfd.load('caltech101', split=['train', 'test'], shuffle_files=True,
                                         as_supervised=True, with_info=True)
 
-train_ds = (ds_train.shuffle(Config.BATCH_SIZE * 100).map(preprocess_caltech101, num_parallel_calls=Config.AUTO)
-            .batch(Config.BATCH_SIZE).prefetch(Config.AUTO))
-ds_test = (ds_test.map(preprocess_caltech101, num_parallel_calls=Config.AUTO).batch(Config.BATCH_SIZE)
-           .prefetch(Config.AUTO))
+train_ds = (ds_train.shuffle(Cfg.BATCH_SIZE * 100).map(preprocess_caltech101, num_parallel_calls=Cfg.AUTO)
+            .batch(Cfg.BATCH_SIZE).prefetch(Cfg.AUTO))
+ds_test = (ds_test.map(preprocess_caltech101, num_parallel_calls=Cfg.AUTO).batch(Cfg.BATCH_SIZE)
+           .prefetch(Cfg.AUTO))
 
 # Load model and compile it
-res_net = ResNet50(input_shape=Config.INPUT_SIZE, classes=Config.CLASSES)
-model = res_net.res_net50()
+# res_net = ResNet50(input_shape=Cfg.RESNET50_INPUT_SIZE, classes=Cfg.CLASSES)
+# model = res_net.res_net50()
 
-model.compile(loss=CategoricalCrossentropy(label_smoothing=0.1), optimizer='adam', metrics=["accuracy"])
+mobile_net = MobileNetV1(input_shape=Cfg.MOBILENET_V1_INPUT_SIZE, classes=Cfg.CLASSES)
+model = mobile_net.mobile_net_v1()
+
+model.compile(loss=CategoricalCrossentropy, optimizer='adam', metrics=["accuracy"])
 
 # Train network
-model.fit(train_ds, validation_data=ds_test, epochs=Config.EPOCHS)
+model.fit(train_ds, validation_data=ds_test, epochs=Cfg.EPOCHS)
