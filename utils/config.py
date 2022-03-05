70d188b33b54f314ab14077c2c15a2f212e6c73b
MrRiahi <mohammad.r.riahi@gmail.com>
2022-03-05 22:52:07 +0330

    Update config file

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,8 @@ class Config:
     # Models config
     RESNET50_INPUT_SIZE = (64, 64)
     MOBILENET_V1_INPUT_SIZE = (224, 224)
-    MODEL_TYPE = 'MobileNetV1'  # 'MobileNetV1', 'ResNet50'
+    GOOGLE_NET_INPUT_SIZE = (224, 224)
+    MODEL_TYPE = 'GoogleNet'  # 'MobileNetV1', 'ResNet50', 'GoogleNet'
 
     # Train config
     BATCH_SIZE = 128
