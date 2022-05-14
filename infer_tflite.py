7afc431e5adcd9032a1ae92ccbc8b070071b2555
riahi <mohammad.r.riahi@gmail.com>
2022-05-14 13:59:59 +0430

    Refactor inference to predict with model input shape

--- infer_tflite.py
+++ infer_tflite.py
@@ -11,16 +11,16 @@ model_path = f'./models/cifar-10/{Cfg.MODEL_TYPE}.tflite'
 interpreter = tf.lite.Interpreter(model_path=model_path)
 interpreter.allocate_tensors()
 
-# Load images
-images_path = ['./samples/11.png']
-images = Uf.load_images(images_path=images_path)
-
-# Convert float64 to float32
-images = images.astype('float32')
-
 # Get input and output tensors
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
+# Load images
+images_path = ['./samples/11.png']
+images = Uf.load_images(images_path=images_path, input_shape=input_details[0]['shape'][1:3])
+
+# Convert float64 to float32
+images = images.astype('float32')
 
 # Infer on a sample image
 interpreter.set_tensor(input_details[0]['index'], images)
