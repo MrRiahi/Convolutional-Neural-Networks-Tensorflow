f648c7536959b560509ea2258f1df5f602639c86
riahi <mohammad.r.riahi@gmail.com>
2022-05-22 15:20:06 +0430

    Fix bug

--- infer_tflite.py
+++ infer_tflite.py
@@ -17,7 +17,7 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Load images
-images_path = ['./samples/11.png']
+images_path = ['./samples/30.png']
 images = Uf.load_images(images_path=images_path, input_shape=input_details[0]['shape'][1:3])
 
 # Convert float64 to float32
@@ -30,6 +30,6 @@ interpreter.invoke()
 output_data = interpreter.get_tensor(output_details[0]['index'])
 
 # Convert output to corresponding label
-label = Cfg.CIFAR_10_CLASS_NAMES[np.argmax(output_data[0][0])]
+label = Cfg.CIFAR_10_CLASS_NAMES[np.argmax(output_data[0])]
 
 print(f'The predicted labels is {label}')
