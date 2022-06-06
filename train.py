07df1532233951781d650ae3f373497ee0326bd5
riahi <mohammad.r.riahi@gmail.com>
2022-06-06 19:10:46 +0430

    Update train

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from tensorflow.keras.losses import CategoricalCrossentropy
 import tensorflow as tf
 import numpy as np
 
-from utils.dataset import get_train_dataset
+from utils.dataset import get_dataset
 from utils.utils import UtilityFunction
 from utils.config import Config as Cfg
 from utils.model import get_model
@@ -11,17 +11,8 @@ from utils.model import get_model
 # Build and compile model
 model, input_size = get_model(classes_numbers=Cfg.CIFAR_10_CLASS_NUMBERS)
 
-# Get training dataset
-train_dataset, validation_dataset = get_train_dataset(
-    directory='./dataset/cifar-10/images/train',
-    classes=Cfg.CIFAR_10_CLASS_NAMES,
-    image_size=input_size,
-    batch_size=Cfg.BATCH_SIZE,
-    class_mode='categorical',
-    color_mode='rgb',
-    shuffle=True,
-    seed=0)
-
+# Get train and val datasets
+train_dataset, val_dataset = get_dataset(input_size=input_size)
 
 # Use callbacks
 model_path = f'./models/cifar-10/{Cfg.MODEL_TYPE}'
@@ -38,7 +29,7 @@ learning_rate = tf.keras.callbacks.LearningRateScheduler(UtilityFunction.step_de
 callbacks = [model_checkpoint_callback, learning_rate]
 
 # Train network
-history = model.fit(train_dataset, validation_data=validation_dataset,
+history = model.fit(train_dataset, validation_data=val_dataset,
                     epochs=Cfg.EPOCHS, callbacks=callbacks)
 
 # Save history
