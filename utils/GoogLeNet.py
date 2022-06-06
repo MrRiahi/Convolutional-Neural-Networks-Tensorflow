69075669629b7da78174e92bd3808cd4f68bf2cc
riahi <mohammad.r.riahi@gmail.com>
2022-06-06 16:42:33 +0430

    Update GoogLeNet

--- utils/GoogLeNet.py
+++ utils/GoogLeNet.py
@@ -27,13 +27,13 @@ class GoogLeNet:
         kernel_size_1by1, kernel_size_3by3, kernel_size_5by5 = kernel_size
 
         X_1by1 = Conv2D(filters=filters_1by1, kernel_size=kernel_size_1by1, strides=(1, 1), activation='relu',
-                        padding='valid', kernel_initializer=initializer(seed=0))(X)
+                        padding='valid', kernel_initializer=initializer())(X)
 
         X_3by3 = Conv2D(filters=filters_3by3, kernel_size=kernel_size_3by3, strides=(1, 1), activation='relu',
-                        padding='valid', kernel_initializer=initializer(seed=0))(X)
+                        padding='valid', kernel_initializer=initializer())(X)
 
         X_5by5 = Conv2D(filters=filters_5by5, kernel_size=kernel_size_5by5, strides=(1, 1), activation='relu',
-                        padding='valid', kernel_initializer=initializer(seed=0))(X)
+                        padding='valid', kernel_initializer=initializer())(X)
 
         X_max_pooling = MaxPooling2D(pool_size=pool_size, strides=(1, 1), padding='same')(X)
 
@@ -61,25 +61,25 @@ class GoogLeNet:
 
         # 1x1 layer
         X_1by1 = Conv2D(filters=filters_1by1, kernel_size=kernel_size_1by1, strides=(1, 1), activation='relu',
-                        padding='same', kernel_initializer=initializer(seed=0))(X)
+                        padding='same', kernel_initializer=initializer())(X)
 
         # 3x3 layer
         X_reduced_3by3 = Conv2D(filters=reduced_filters_3by3, kernel_size=(1, 1), strides=(1, 1),
-                                activation='relu', padding='same', kernel_initializer=initializer(seed=0))(X)
+                                activation='relu', padding='same', kernel_initializer=initializer())(X)
         X_3by3 = Conv2D(filters=filters_3by3, kernel_size=kernel_size_3by3, strides=(1, 1), activation='relu',
-                        padding='same', kernel_initializer=initializer(seed=0))(X_reduced_3by3)
+                        padding='same', kernel_initializer=initializer())(X_reduced_3by3)
 
         # 5x5 layer
         X_reduced_5by5 = Conv2D(filters=reduced_filters_5by5, kernel_size=(1, 1), strides=(1, 1),
-                                activation='relu', padding='same', kernel_initializer=initializer(seed=0))(X)
+                                activation='relu', padding='same', kernel_initializer=initializer())(X)
         X_5by5 = Conv2D(filters=filters_5by5, kernel_size=kernel_size_5by5, strides=(1, 1), activation='relu',
-                        padding='same', kernel_initializer=initializer(seed=0))(X_reduced_5by5)
+                        padding='same', kernel_initializer=initializer())(X_reduced_5by5)
 
         # max pooling layer
         X_max_pooling = MaxPooling2D(pool_size=pool_size, strides=(1, 1), padding='same')(X)
         X_reduced_5by5 = Conv2D(filters=pool_projection, kernel_size=(1, 1), strides=(1, 1),
                                 activation='relu', padding='same',
-                                kernel_initializer=initializer(seed=0))(X_max_pooling)
+                                kernel_initializer=initializer())(X_max_pooling)
 
         # concatenate layers
         X_concat = concatenate(inputs=[X_1by1, X_3by3, X_5by5, X_reduced_5by5])
@@ -100,20 +100,20 @@ class GoogLeNet:
 
         # Convolution layer for dimensionality reduction
         X_conv = Conv2D(filters=128, kernel_size=(1, 1), strides=(1, 1), padding='same', activation='relu',
-                        kernel_initializer=initializer(seed=0))(X_average_pool)
+                        kernel_initializer=initializer())(X_average_pool)
 
         # Flatten layer
         X_flatten = Flatten()(X_conv)
 
         # FC layer
-        X_fc = Dense(units=1024, activation='relu', kernel_initializer=glorot_uniform(seed=0))(X_flatten)
+        X_fc = Dense(units=1024, activation='relu', kernel_initializer=glorot_uniform())(X_flatten)
 
         # Dropout layer
         X_dropout = Dropout(rate=0.7)(X_fc)
 
         # Auxiliary output layer
         X_aux_output = Dense(units=self.classes, activation='softmax', name=output_name,
-                             kernel_initializer=glorot_uniform(seed=0))(X_dropout)
+                             kernel_initializer=glorot_uniform())(X_dropout)
 
         return X_aux_output
 
@@ -158,44 +158,44 @@ class GoogLeNet:
         # First auxiliary output
         X_aux_1 = self._auxiliary_classifier(X=X, output_name='output_aux_1')
 
-        # Inception 4b
-        X = self._inception_block(X=X, filters=(160, 224, 64), reduced_filters=(112, 24, 64))
-
-        # Inception 4c
-        X = self._inception_block(X=X, filters=(128, 256, 64), reduced_filters=(128, 24, 64))
-
-        # Inception 4d
-        X = self._inception_block(X=X, filters=(112, 288, 64), reduced_filters=(144, 32, 64))
-
-        # Second auxiliary output
-        X_aux_2 = self._auxiliary_classifier(X=X, output_name='output_aux_2')
-
-        # Inception 4e
-        X = self._inception_block(X=X, filters=(256, 320, 128), reduced_filters=(160, 32, 128))
-
-        # Layer 14
-        X = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(X)
-
-        # Inception 5a
-        X = self._inception_block(X=X, filters=(256, 320, 128), reduced_filters=(160, 32, 128))
-
-        # Inception 5b
-        X = self._inception_block(X=X, filters=(384, 384, 128), reduced_filters=(192, 48, 128))
-
-        # Layer 17
-        X = AveragePooling2D(pool_size=(7, 7), strides=(1, 1), padding='valid')(X)
-        X = Flatten()(X)
-
-        # Layer 18
-        X = Dropout(rate=0.4)(X)
-
-        # Layer 19
-        X = Dense(units=1000, activation='relu', kernel_initializer=glorot_uniform(seed=0))(X)
-
-        # Layer 20
-        output = Dense(units=self.classes, activation='softmax', kernel_initializer=glorot_uniform(seed=0))(X)
+        # # Inception 4b
+        # X = self._inception_block(X=X, filters=(160, 224, 64), reduced_filters=(112, 24, 64))
+        #
+        # # Inception 4c
+        # X = self._inception_block(X=X, filters=(128, 256, 64), reduced_filters=(128, 24, 64))
+        #
+        # # Inception 4d
+        # X = self._inception_block(X=X, filters=(112, 288, 64), reduced_filters=(144, 32, 64))
+        #
+        # # Second auxiliary output
+        # # X_aux_2 = self._auxiliary_classifier(X=X, output_name='output_aux_2')
+        #
+        # # Inception 4e
+        # X = self._inception_block(X=X, filters=(256, 320, 128), reduced_filters=(160, 32, 128))
+        #
+        # # Layer 14
+        # X = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(X)
+        #
+        # # Inception 5a
+        # X = self._inception_block(X=X, filters=(256, 320, 128), reduced_filters=(160, 32, 128))
+        #
+        # # Inception 5b
+        # X = self._inception_block(X=X, filters=(384, 384, 128), reduced_filters=(192, 48, 128))
+        #
+        # # Layer 17
+        # X = AveragePooling2D(pool_size=(7, 7), strides=(1, 1), padding='valid')(X)
+        # X = Flatten()(X)
+        #
+        # # Layer 18
+        # X = Dropout(rate=0.4)(X)
+        #
+        # # Layer 19
+        # X = Dense(units=1000, activation='relu', kernel_initializer=glorot_uniform())(X)
+        #
+        # # Layer 20
+        # output = Dense(units=self.classes, activation='softmax', kernel_initializer=glorot_uniform())(X)
 
         # Create model
-        model = Model(inputs=X_input, outputs=[output, X_aux_1, X_aux_2])
+        model = Model(inputs=X_input, outputs=X_aux_1)
 
         return model
