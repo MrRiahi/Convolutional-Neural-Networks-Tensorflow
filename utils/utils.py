a58fe120dcc402b52206fc0ef38a5972f19c0c06
riahi <mohammad.r.riahi@gmail.com>
2022-06-06 16:44:23 +0430

    Update learning rate scheduler

--- utils/utils.py
+++ utils/utils.py
@@ -10,9 +10,10 @@ class UtilityFunction:
     @staticmethod
     def step_decay_classification(epoch):
         initial_lr = 0.1
-        drop = 0.5
-        epochs_drop = 100.0
+        drop = 0.04
+        epochs_drop = 8
         lr = initial_lr * math.pow(drop, math.floor((1 + epoch) / epochs_drop))
+        print(f'------------learning rate:: {lr}')
         return lr
 
     @staticmethod
