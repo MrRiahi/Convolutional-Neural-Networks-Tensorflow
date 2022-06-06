8c83ba747d9e715d6157f9d165a1787d99823ac1
riahi <mohammad.r.riahi@gmail.com>
2022-06-06 16:42:48 +0430

    Update config

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ class Config:
     MOBILENET_V2_INPUT_SIZE = (224, 224)
     GOOGLE_NET_INPUT_SIZE = (224, 224)
 
-    MODEL_TYPE = 'MobileNetV1'  # 'MobileNetV1', 'MobileNetV2', 'ResNet50', 'GoogLeNet'
+    MODEL_TYPE = 'GoogLeNet'  # 'MobileNetV1', 'MobileNetV2', 'ResNet50', 'GoogLeNet'
 
     # Train config
     BATCH_SIZE = 128
