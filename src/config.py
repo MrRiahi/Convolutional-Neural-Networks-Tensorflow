53938aaebccd145f01594c09c3a0c874eb0aae0b
riahi <mohammad.r.riahi@gmail.com>
2022-07-02 11:58:14 +0430

    Update config

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class Config:
     VGG11_NET_INPUT_SIZE = (224, 224)
 
     # 'MobileNetV1', 'MobileNetV2', 'ResNet50', 'GoogLeNet', 'VGG16', 'VGG13', 'VGG11', 'InceptionV2'
-    MODEL_TYPE = 'ResNet50'
+    MODEL_TYPE = 'MobileNetV2'
 
     # Train config
     BUFFER_SIZE = 500
