7be9fce04fa2d4141273ed30852ab4a13df4ec53
riahi <mohammad.r.riahi@gmail.com>
2022-08-06 17:45:28 +0430

    Update the model path

--- convert.py
+++ convert.py
@@ -3,7 +3,8 @@ from src.config import Config as Cfg
 
 
 # Define model path
-model_path = f'./models/cifar-10/{Cfg.MODEL_TYPE}'
+# model_path = f'./models/cifar-10/{Cfg.MODEL_TYPE}'
+model_path = f'{Cfg.MODEL_PATH}/best'
 
 # Convert and save model to 'TFLite' or 'onnx'
 output_model_directory = convert_model(model_directory=model_path, output_type='TFLite')
