eae0ef848fcabfe48ff31aa9857773ac80bf2697
riahi <mohammad.r.riahi@gmail.com>
2022-08-14 18:39:18 +0430

    Rename the best filename

--- fine_tuning.py
+++ fine_tuning.py
@@ -13,7 +13,7 @@ model, input_shape = load_model(model_path=f'{Cfg.MODEL_PATH}/last')
 train_dataset, val_dataset = get_train_dataset(input_shape=input_shape)
 
 # Use ModelCheckpoint to control validation loss for saving the best model.
-best_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=Cfg.MODEL_PATH + '/best.val_loss{val_loss:.2f}',
+best_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=Cfg.MODEL_PATH + '/best.val_loss_{val_loss:.2f}',
                                                               monitor='val_loss',
                                                               verbose=1,
                                                               save_best_only=True)
